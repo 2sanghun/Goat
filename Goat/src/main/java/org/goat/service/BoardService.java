168646d3a069886e99810b2d59ae585d365b3c27
GreenArt <GreenArt@GRN-401-05>
2022-08-09 11:42:05 +0900

    SQL 연결

---
+++ Goat/src/main/java/org/goat/service/BoardService.java
@@ -0,0 +1,13 @@
+package org.goat.service;
+
+import org.goat.model.BoardVO;
+
+public interface BoardService {
+	// 글쓰기 설계
+	public void boardwrite(BoardVO board);
+	// 글 목록 리스트 설계
+	// 글 상세 페이지 설계
+	// 글 수정 설계
+	// 글 삭제 설계
+
+}
