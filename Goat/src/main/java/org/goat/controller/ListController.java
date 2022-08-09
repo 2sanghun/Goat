eb5f0b4e8421cd1d99cb7a6b5f71359fc9926f6a
GreenArt <olim84m@gmail.com>
2022-08-10 08:44:17 +0900

    별거없음

--- Goat/src/main/java/org/goat/controller/ListController.java
+++ Goat/src/main/java/org/goat/controller/ListController.java
@@ -1,12 +1,14 @@
 package org.goat.controller;
+import org.goat.model.BoardVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ListController {
+	
 	@RequestMapping(value = "/list/list", method = RequestMethod.GET)
-	public void list() {
+	public void list(BoardVO board) {
 		
 	}
 	@RequestMapping(value = "/list/foodlist", method = RequestMethod.GET)
