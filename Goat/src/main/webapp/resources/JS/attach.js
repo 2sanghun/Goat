4071461754618e13312c933fa4e1e113c6d4544b
GR <GR@DESKTOP-ODLO1F7>
2022-09-01 15:16:53 +0900

    디테일에 업로드 파일 표시

--- Goat/src/main/webapp/resources/JS/attach.js
+++ Goat/src/main/webapp/resources/JS/attach.js
@@ -4,18 +4,20 @@
 $(document).ready(function() {
 	// detail.jsp가 시작되자마자(ready이벤트) bno값 가져오기
 	var bno=$("input[name='bno']").val();
-	alert(bno);
+	// alert(bno);
+	
 	$.getJSON("/attachlist",{bno:bno},function(attachlist){
 		console.log(attachlist);
 		var str="";
+		
 		$(attachlist).each(function(i,attach){
 			// 만약 image결과가 true면
 			if(attach.image){
 				var filePath = encodeURIComponent(attach.uploadPath+"/s_"+attach.uuid+"_"+attach.fileName);
-				str+="<li><img src='display?fileName="+filePath+"'></li>";
+				str+="<li><img src='/display?fileName="+filePath+"'></li>";
 			}else{	// 그렇지 않으면
 				var filePath = encodeURIComponent(attach.uploadPath+"/"+attach.uuid+"_"+attach.fileName);
-				str+="<li><a href='download?fileName="+filePath+"'>"+attach.fileName+"</a></li>";
+				str+="<li><a href='/download?fileName="+filePath+"'>"+attach.fileName+"</a></li>";
 			}
 		})
 		$("#uploadResult ul").html(str);
