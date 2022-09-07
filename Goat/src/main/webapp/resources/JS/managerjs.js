18c686d40da14948fdbf6afe8a99079cf8ad6c12
GR <GR@DESKTOP-ODLO1F7>
2022-09-07 10:51:52 +0900

    1

--- Goat/src/main/webapp/resources/JS/managerjs.js
+++ Goat/src/main/webapp/resources/JS/managerjs.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 	})
 
 	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0}, 600);
+		$('html, body').animate({scrollTop : 0}, 300);
 		return false;
 		});
 	
